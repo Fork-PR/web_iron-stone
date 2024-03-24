796f9810c57ec96b9ffa246adae2b160579936d4
CSchoice <tkdlqjf@example.com>
2024-03-24 19:57:09 +0900

    Feat: views 파일에 index, menuList함수 추가, 회원가입 함수 주석처리, urls.py 미사용 경로 주석처, user_login 함수 로그인 성공 시 새 html로 이동하도록 수정

--- bobapps/urls.py
+++ bobapps/urls.py
@@ -3,8 +3,11 @@ from bobapps import views
 
 app_name = 'bobapps'
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.user_login, name='login'),
-    path('menuList', views.menuList, name='menuList')
+    #회원가입 미구현
+    # path('signup/', views.signup, name='signup'),
+    #index에서 같이 사용할 예정
+    # path('login/', views.user_login, name='login'),
+    # path('menuList', views.menuList, name='menuList'),
+    path('', views.index, name='index'),
     # other urls...
 ]

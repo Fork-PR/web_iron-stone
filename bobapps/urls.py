351bc1358a9234df1c93d293fd5e982f11708024
CSquokka <tkdlqjf@gmail.com>
2024-03-25 12:01:23 +0900

    Fix: 로그인함수 return 형식으로 json형식으로 보냄

--- bobapps/urls.py
+++ bobapps/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     #회원가입 미구현
     # path('signup/', views.signup, name='signup'),
     #index에서 같이 사용할 예정
-    # path('login/', views.user_login, name='login'),
-    # path('menuList', views.menuList, name='menuList'),
+    path('login/', views.user_login, name='login'),
+    path('menuList', views.menuList, name='menuList'),
     path('', views.index, name='index'),
     # other urls...
 ]
--- bobapps/views.py
+++ bobapps/views.py
@@ -6,13 +6,6 @@ from django.contrib.staticfiles import finders
 import json
 
 def index(request):
-    if request.method == 'POST':
-        # POST 요청이 오면 user_login 함수를 호출하여 로그인을 시도
-        # 로그인 성공하면 성공 페이지로 이동
-        # 로그인이 실패하면 index 페이지에서 에러문구 출력
-        return user_login(request)
-    else:
-        # POST 요청이 아니면 index.html 보여주기
         return render(request, 'index.html')
 
 
@@ -44,14 +37,14 @@ def user_login(request):
                 'password' : password,
             }
             #render에 context를 인자로 줘서 딕셔너리로 전달
-            return render(request, '이동할 템플릿.html', context)
+            return JsonResponse(context)
         else:
             # 사용자가 존재하지 않을 때
             if not authenticate(username=username):
-                return render(request, 'index.html', {'message': '해당 ID가 없습니다.'})
+                return JsonResponse({'message': '해당 ID가 없습니다.'}, status=401)
             # 비밀번호가 틀렸을 때
             elif not authenticate(username=username, password=password):
-                return render(request, 'index.html', {'message': '비밀번호가 틀렸습니다.'})
+                return JsonResponse({'message': '비밀번호가 틀렸습니다.'}, status=401)
 
 
 def menuList(request):

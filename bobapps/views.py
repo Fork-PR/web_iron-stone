d7a1c27126d7dc531990b75673706f2aad29e0df
CSquokka <tkdlqjf@gmail.com>
2024-03-28 09:18:42 +0900

    Feat: views.py menu_type을 menu_course_type으로 변경, save_menu 함수에서 서브 메뉴를 추가하는 부분에서 get_or_create 함수를 위해 왼쪽에 sub_create를 추가

--- bobapps/views.py
+++ bobapps/views.py
@@ -112,18 +112,18 @@ def save_menu(request):
     if request.method == 'POST':
         # POST 요청을 받았을 때 데이터 처리
         date = request.POST.get('date')
-        menu_type = request.POST.get('menu_type')
+        menu_course_type = request.POST.get('menu_course_type')
         main_dish = request.POST.get('main_dish')
         # 서브 메뉴는 여러 개일 수 있으므로 리스트로 받음
         sub_menus = request.POST.getlist('sub_menus')
 
         # Menu 객체 생성
         #created는 서브메뉴가 생성 되었는지 여부를 나타낸다.(bool값)
-        menu, create = Menu.objects.get_or_create(date=date, menu_type=menu_type, main_dish=main_dish)
+        menu, create = Menu.objects.get_or_create(date=date, menu_course_type=menu_course_type, main_dish=main_dish)
 
         # 서브 메뉴를 추가
         for sub_menu_name in sub_menus:
-            sub_menu = SubMenu.objects.get_or_create(name=sub_menu_name)
+            sub_menu, sub_create = SubMenu.objects.get_or_create(name=sub_menu_name)
             menu.sub_menus.add(sub_menu)
         if create==True:
             return render(request, 'menu_form.html', {'message': '메뉴가 추가되었습니다.'})

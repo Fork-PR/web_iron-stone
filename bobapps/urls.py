ecdced733379c6af90ba6c0bf039ece71d9f8f80
k-redstone <79430840+k-redstone@users.noreply.github.com>
2024-04-01 22:39:15 +0900

    Update urls.py

--- bobapps/urls.py
+++ bobapps/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from bobapps import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'bobapps'
 urlpatterns = [
@@ -13,3 +15,5 @@ urlpatterns = [
     path('save_menu/', views.save_menu, name='save_menu')
     # other urls...
 ]
+
+urlpatterns += static(settings.STATIC_URL, domcument_root=settings.STATIC_ROOT)
